9ecb071d9060b8fadfc294bf922dee00ec9c8a8b
IrfanulArifa <57009203+IrfanulArifa@users.noreply.github.com>
2019-10-31 06:49:46 -0700

    Update RumusJajarGenjang.m

--- RumusJajarGenjang.m
+++ RumusJajarGenjang.m
@@ -6,5 +6,5 @@ tinggi6 = input('Masukkan Tinggi : ');
 miring6 = input('Masukkan Panjang sisi Miring : ');
 Keliling6 = 2*Alas6+2*miring6;
 Luas6 = Alas6*tinggi6;
-disp("Keliling Belah Ketupat : "+Keliling6);
-disp("Luas     Belah Ketupat : "+Luas6);
+disp("Keliling Jajar Genjang : "+Keliling6);
+disp("Luas     Jajar Genjang : "+Luas6);
